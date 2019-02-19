bc6b72eb577d545336ac7dfeeca8c03fe6956f89
Quy Nguyen <quycao1510@gmail.com>
2019-02-19 21:04:48 +0700

    Add Header and Footer for Homepage component

--- client/shop_ui/src/components/Homepage.js
+++ client/shop_ui/src/components/Homepage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import "../style/style.css";
 
 
 class HomePage extends Component {
   render() {
     return (
         <div>
-           <h1>Homepage</h1>
+           <Header/>
+           <Footer/>
         </div>
     );
   }
