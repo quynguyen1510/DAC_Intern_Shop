69d90fd0316d83373a9042103fd8a85762b12f9a
Quy Nguyen <quycao1510@gmail.com>
2019-02-19 21:02:46 +0700

    Add Footer component

---
+++ client/shop_ui/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer id="footer" className="container-fluid text-center">
+                <p>This is footer</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
