4b393a23ca7568d2d74ab1f28dfa895a933a72fd
lap14tclc2 <honaz140@gmail.com>
2019-02-20 13:46:09 +0700

    add routes for homepage

---
+++ client/shop_ui/src/routes/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from '../components/Homepage';
+import { Route } from 'react-router';
+
+export default (
+    <Route path="/" component={HomePage}>
+    </Route>
+);
