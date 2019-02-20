1970918aca1e98e4dbb8a67d13a0f5fc61f0fc2b
lap14tclc2 <honaz140@gmail.com>
2019-02-20 11:53:45 +0700

    create login actions and api login method

---
+++ client/shop_ui/src/actions/sesson/SessionAction.js
@@ -0,0 +1,20 @@
+import { LOG_IN_SUCCESS } from './actionTypes';
+import sessionApi from '../../api/SessionApi';
+
+export function loginSuccess(){
+    return{
+        type: LOG_IN_SUCCESS
+    }
+}
+
+export function login(crendentials){
+    return function(dispatch){
+        // if login successfully then get token and save it to local storage
+        return sessionApi.login(crendentials).then(response => {
+            sessionStorage.setItem('token', response.auth_token)
+            dispatch(loginSuccess());
+        }).catch(error => {
+            throw(error);
+        })
+    }
+}
