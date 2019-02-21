586559bd4fcaa0a606ed5c730c40abd3814271e0
lap14tclc2 <honaz140@gmail.com>
2019-02-21 15:36:45 +0700

    change routes

--- server/shop_api/app/controllers/users_controller.rb
+++ server/shop_api/app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create  
 
+  # GET '/users'
+  def index 
+  end
+
   # POST '/signup'
   def create 
     # use create! method will raise RecordInvalid exception if credentials is invalid
     # add normal user role for signup user
     data = user_params
     data[:role_id] = 2
-    puts data
 
     @user = User.create!(data)
     response = { 
@@ -17,8 +20,9 @@ class UsersController < ApplicationController
     json_response(response, :created)
   end
 
-   private 
+  private 
   def user_params
     params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
+
 end
