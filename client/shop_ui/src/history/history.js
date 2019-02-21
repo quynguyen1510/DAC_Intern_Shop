05d0465d32673f4494507a29be8b0a13ccceefde
lap14tclc2 <honaz140@gmail.com>
2019-02-21 09:00:00 +0700

    refactor code and change header after user login

--- client/shop_ui/src/history/history.js
+++ client/shop_ui/src/history/history.js
@@ -1,8 +1,4 @@
 import {createBrowserHistory} from 'history';
-import {BrowserRouter} from 'react-router-dom';
 
 export const history = createBrowserHistory();
 
-export default class CustomBrowserRouter extends BrowserRouter {
-    history
-}
