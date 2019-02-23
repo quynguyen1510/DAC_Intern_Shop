888523bf3c2435e29d0bba21e99d253fe70f02ac
Quy Nguyen <quycao1510@gmail.com>
2019-02-23 20:21:55 +0700

    Add Campaign component

--- client/shop_ui/src/components/Homepage.js
+++ client/shop_ui/src/components/Homepage.js
@@ -3,25 +3,27 @@ import React, { Component } from 'react';
 import "../style/style.css";
 import Header from './commons/header/Header';
 import Footer from './commons/footer/Footer';
+import Campaign from './commons/body/Campaign';
 import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router';
-import {login ,signup}  from '../actions/SessionAction';
-import {getAuthenticatedUser} from '../actions/UsersAction';
+import { login, signup } from '../actions/SessionAction';
+import { getAuthenticatedUser } from '../actions/UsersAction';
 
 
 class HomePage extends Component {
   render() {
     return (
-        <div className="wrapper">
-           <Header {...this.props}/>
-           <Footer />
-        </div>
+      <div className="wrapper">
+        <Header {...this.props} />
+        <Campaign />
+        <Footer />
+      </div>
     );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
     session: state
   }
---
+++ client/shop_ui/src/components/commons/body/Campaign.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Campaign extends Component {
+    render() {
+        return (
+            <div className="campaign-page">
+                <div>
+                    <a href="#none" class="btn btn-default" id="btnCreateCampaign">Create Campaign</a>
+                </div>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Campaign Name</th>
+                            <th>Status</th>
+                            <th>Start date</th>
+                            <th>End date</th>
+                            <th>Budget</th>
+                            <th>Bid amount</th>
+                            <th>Creative Preview</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Doe</td>
+                            <td>Active</td>
+                            <td>02/09/2019</td>
+                            <td>02/10/2019</td>
+                            <td>100</td>
+                            <td>100</td>
+                            <td>Giảm giá</td>
+                            <td>Siêu giảm giá lễ quốc khánh</td>
+                            <td>Doe</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Campaign;
