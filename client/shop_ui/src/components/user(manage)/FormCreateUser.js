669b187a330deea3018b027f14741f64011ec88c
Quy Nguyen <quycao1510@gmail.com>
2019-02-24 16:52:52 +0700

    Format code for FormCreateUser component

--- client/shop_ui/src/components/user(manage)/FormCreateUser.js
+++ client/shop_ui/src/components/user(manage)/FormCreateUser.js
@@ -5,43 +5,43 @@ class FormCreateUser extends Component {
         return (
             <div>
                 <form>
-                    <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="">First and last name</span>
+                    <div className="input-group mb-4">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="">First and last name</span>
                         </div>
-                        <input type="text" class="form-control" />
-                        <input type="text" class="form-control" />
+                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" />
                     </div>
-                    <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="">Email address</span>
+                    <div className="input-group mb-4">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="">Email address</span>
                         </div>
-                        <input type="email" class="form-control" placeholder="david@gmail.com"/>
+                        <input type="email" className="form-control" placeholder="david@gmail.com"/>
                     </div>
-                    <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="">Password and Password Confirm</span>
+                    <div className="input-group mb-4">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="">Password and Password Confirm</span>
                         </div>
-                        <input type="password" class="form-control" />
-                        <input type="password" class="form-control" />
+                        <input type="password" className="form-control" />
+                        <input type="password" className="form-control" />
                     </div>
-                    <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="">Role</span>
+                    <div className="input-group mb-4">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="">Role</span>
                         </div>
-                        <select class="form-control" id="exampleFormControlSelect1">
+                        <select className="form-control" id="exampleFormControlSelect1">
                             <option>Admin</option>
                             <option>Shop</option>
                             <option>User</option>
                         </select>
                     </div>
-                     <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="">Add Avatar</span>
+                     <div className="input-group mb-4">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="">Add Avatar</span>
                         </div>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+                        <input type="file" className="form-control-file" id="exampleFormControlFile1" />
                     </div>
-                    <button type="submit" class="btn btn-primary mb-4">Create</button>
+                    <button type="submit" className="btn btn-primary mb-4">Create</button>
                 </form>
             </div>
         );
