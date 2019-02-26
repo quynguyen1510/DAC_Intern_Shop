2f39f1ce7d512087fea14159c4c56ee0f11e3f41
Quy Nguyen <quycao1510@gmail.com>
2019-02-26 16:36:53 +0700

    Add Banner and Categoris component

--- client/shop_ui/src/components/commons/body/Body.js
+++ client/shop_ui/src/components/commons/body/Body.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Sidebar from './Sidebar';
+import Categories from './Categories'; 
+import Banner from './SlideBanner';
 class Body extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                  <div id="banner">
-                  </div>
+            <div className="container-fluid cat-banner">
+                <div className="row justify-content-center cat-banner">
+                    <Categories/>
+                    <Banner/>
                 </div>
             </div>
         );
---
+++ client/shop_ui/src/components/commons/body/Categories.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Categories extends Component {
+    render() {
+        return (
+            <div id="categories">
+            <ul>
+                <li>
+                    <a href="#none">Thiết Bị Điện Tử</a>
+                </li>
+                <li>
+                    <a href="#none">Phụ Kiện Điện Tử</a>
+                </li>
+                <li>
+                    <a href="#none">TV & Thiết Bị Gia Dụng</a>
+                </li>
+                <li>
+                    <a href="#none">Sức Khỏe & Làm Đẹp</a>
+                </li>
+                <li>
+                    <a href="#none">Hàng Mẹ,Bé & Đồ Chơi</a>
+                </li>
+                <li>
+                    <a href="#none">Siêu Thị Tạp Hóa</a>
+                </li>
+                <li>
+                    <a href="#none">Hàng Gia Dụng & Đời Sống</a>
+                </li>
+                <li>
+                    <a href="#none">Thời Trang</a>
+                </li>
+                <li>
+                    <a href="#none">Du Lịch</a>
+                </li>
+            </ul>
+        </div>
+        );
+    }
+}
+
+export default Categories;
