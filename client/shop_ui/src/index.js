c5d379043a27ecb7b6f6f8baeb09bac4cc4b47e0
lap14tclc2 <honaz140@gmail.com>
2019-02-26 15:45:13 +0700

    refactor Header and index.js

--- client/shop_ui/src/index.js
+++ client/shop_ui/src/index.js
@@ -3,22 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {store, persistor} from './store/ConfigStore';
 import { Provider } from 'react-redux';
-import HomePageContainer from './components/Homepage';
-import {BrowserRouter , Route} from 'react-router-dom';
-import Profile from './components/users/Profie';
-import ManageUser from './components/users/ManageUser';
+import {BrowserRouter} from 'react-router-dom';
 import {PersistGate} from 'redux-persist/integration/react';
-import CreateUser from './components/users/CreateUser';
+
+import ProtectedRoutesContainer from './routes/ProtectedRoute';
+
 
 ReactDOM.render(<Provider store={store}>
                     <PersistGate loading={null} persistor={persistor}>
                         <BrowserRouter >
-                            <div>
-                                <Route path="/" exact component={HomePageContainer} />
-                                <Route path="/profile/users/:id" component={Profile} />
-                                <Route path="/manage/users/:page_number" component={ManageUser}/>
-                                <Route path="/users/new" component={CreateUser}/>
-                            </div>
+                            <ProtectedRoutesContainer/>
                         </BrowserRouter>
                     </PersistGate>
                 </Provider>, 
