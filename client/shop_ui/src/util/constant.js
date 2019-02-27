dc91c2967c591c4ff3502ce82aa8de5ab2ba0d3f
lap14tclc2 <honaz140@gmail.com>
2019-02-27 12:03:20 +0700

    change record of users per page

--- client/shop_ui/src/util/constant.js
+++ client/shop_ui/src/util/constant.js
@@ -2,4 +2,4 @@ export const ADMIN_ROLE = 1;
 export const USER_ROLE = 2;
 export const SHOPPER_ROLE = 3;
 export const GUEST_ROLE = 0;
-export const RECORD_PER_PAGE = 4;
+export const RECORD_PER_PAGE = 10;
