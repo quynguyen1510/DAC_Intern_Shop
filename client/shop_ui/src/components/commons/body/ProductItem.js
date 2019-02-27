9e2d9d5c2ac8212179f5ba1e414da948acff6611
quynguyen1510 <quycao1510@gmail.com>
2019-02-27 13:54:23 +0700

    Add ProductItem component and css for ProductItem

---
+++ client/shop_ui/src/components/commons/body/ProductItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ProductItem extends Component {
+    render() {
+        return (
+            <div className="col-md-2 ml-4 mr-4 mb-2 col-cus">
+                <div className="product-item">
+                    <div className="pi-img-wrapper">
+                        <a href="#none">
+                            <img src="http://keenthemes.com/assets/bootsnipp/k1.jpg" className="img-responsive" alt="Berry Lace Dress" width="100%" height="241px" />
+                        </a>
+                    </div>
+                    <a href="#none" className="item-name">
+                        <h3 className="text-center">Berry Lace Dress</h3>
+                    </a>
+                    <div>
+                        <p className="item-price">$29.00</p>
+                        <a href="#none" className="btn add-cart">Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProductItem;
