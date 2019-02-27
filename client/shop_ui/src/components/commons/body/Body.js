6c43f7b8ac376cd9dc827229c8bd2c9be08f9931
quynguyen1510 <quycao1510@gmail.com>
2019-02-27 11:32:52 +0700

    Add BodyPage component and css for BodyPage

--- client/shop_ui/src/components/commons/body/Body.js
+++ client/shop_ui/src/components/commons/body/Body.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import Categories from './Categories'; 
+import Categories from './Categories';
 import Banner from './SlideBanner';
+import BodyPage from './BodyPage';
+
 class Body extends Component {
     render() {
         return (
-            <div className="container-fluid cat-banner">
-                <div className="row justify-content-center cat-banner">
-                    <Categories/>
-                    <Banner/>
+            <div className="main-content">
+                <div className="container-fluid cat-banner">
+                    <div className="row justify-content-center cat-banner">
+                        <Categories />
+                        <Banner />
+                    </div>
                 </div>
+                <BodyPage />
             </div>
         );
     }
---
+++ client/shop_ui/src/components/commons/body/BodyPage.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+class BodyPage extends Component {
+    render() {
+        return (
+            <div className="container-fluid mb-5 mt-5" id="body-page">
+                <div className="row justify-content-center">
+                    <div className="col-md-2 ml-4 mr-4 mb-2 col-cus">
+                        <div className="product-item">
+                            <div className="pi-img-wrapper">
+                                <a href="#none">
+                                    <img src="http://keenthemes.com/assets/bootsnipp/k1.jpg" className="img-responsive" alt="Berry Lace Dress" width="100%" height="241px" />
+                                </a>
+                            </div>
+                            <a href="#none" className="item-name">
+                                <h3 className="text-center">Berry Lace Dress</h3>
+                            </a>
+                            <div>
+                                <p className="item-price">$29.00</p>
+                                <a href="#none" className="btn add-cart">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-2 ml-4 mr-4 mb-2 col-cus">
+                        <div className="product-item">
+                            <div className="pi-img-wrapper">
+                                <a href="#none">
+                                    <img src="http://keenthemes.com/assets/bootsnipp/k1.jpg" className="img-responsive" alt="Berry Lace Dress" width="100%" height="241px" />
+                                </a>
+                            </div>
+                            <a href="#none" className="item-name">
+                                <h3 className="text-center">Berry Lace Dress</h3>
+                            </a>
+                            <div>
+                                <p className="item-price">$29.00</p>
+                                <a href="#none" className="btn add-cart">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-2 ml-4 mr-4 mb-2 col-cus">
+                        <div className="product-item">
+                            <div className="pi-img-wrapper">
+                                <a href="#none">
+                                    <img src="http://keenthemes.com/assets/bootsnipp/k1.jpg" className="img-responsive" alt="Berry Lace Dress" width="100%" height="241px" />
+                                </a>
+                            </div>
+                            <a href="#none" className="item-name">
+                                <h3 className="text-center">Berry Lace Dress</h3>
+                            </a>
+                            <div>
+                                <p className="item-price">$29.00</p>
+                                <a href="#none" className="btn add-cart">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-2 ml-4 mr-4 mb-2 col-cus">
+                        <div className="product-item">
+                            <div className="pi-img-wrapper">
+                                <a href="#none">
+                                    <img src="http://keenthemes.com/assets/bootsnipp/k1.jpg" className="img-responsive" alt="Berry Lace Dress" width="100%" height="241px" />
+                                </a>
+                            </div>
+                            <a href="#none" className="item-name">
+                                <h3 className="text-center">Berry Lace Dress</h3>
+                            </a>
+                            <div>
+                                <p className="item-price">$29.00</p>
+                                <a href="#none" className="btn add-cart">Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BodyPage;
