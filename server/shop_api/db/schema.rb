defbbd9cf5b36ee96bdbea5bb0f256be12994387
lap14tclc2 <honaz140@gmail.com>
2019-03-04 11:00:44 +0700

    add new column 'token' to users table

--- server/shop_api/db/schema.rb
+++ server/shop_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_035347) do
+ActiveRecord::Schema.define(version: 2019_03_04_035853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_035347) do
     t.bigint "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
