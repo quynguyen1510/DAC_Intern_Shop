d218a98434dfb70863c16f08472d87f78956d842
quynguyen1510 <quycao1510@gmail.com>
2019-03-05 11:01:54 +0700

    Add check shopper for products

--- server/shop_api/app/controllers/concerns/user_helper.rb
+++ server/shop_api/app/controllers/concerns/user_helper.rb
@@ -1,6 +1,10 @@
 module UserHelper
   def is_admin?(user)
-     user.role.role_name == Constants.admin
+    user.role.role_name == Constants.admin
+  end
+
+  def is_shop?(user)
+    user.role.role_name == Constants.shop
   end
 
   def correct_user?(current_user, request_user)
