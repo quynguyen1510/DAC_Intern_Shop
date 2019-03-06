5491186a86ea533dd1544d73d60a29956acf63b3
lap14tclc2 <honaz140@gmail.com>
2019-03-06 15:28:13 +0700

    refactor and add function for button Next and Previous in ManageProduct component

--- client/shop_ui/src/components/products/ManageProduct.js
+++ client/shop_ui/src/components/products/ManageProduct.js
@@ -3,29 +3,57 @@ import ProductTable from './ProductTable';
 import Navbar from '../commons/header/Navbar';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
+import { getListProduct } from '../../api/product_api';
 
 class ManageProduct extends Component {
-    
+
     constructor(props) {
         super(props)
         this.state = {
-            products: []
+            products: [],
+            page: 1,
+            shouldNavigatePage: true,
         }
     }
 
+    loadProduct = (page) => {
+        getListProduct(page).then(res => {
+            if (res.data.length > 0) {
+                this.setState({ products: res.data });
+            }
+            else {
+                this.setState({ shouldNavigatePage: false, page: this.state.page - 1 })
+            }
+        }).catch(err => {
+            console.log(err)
+        });
+    }
+
     componentDidMount() {
-        axios({
-            url: `http://localhost:3000/products/`,
-            method: "GET"
-        }).then(response => {
-            this.setState({
-                products: response.data
-            })
-        }).catch(error => console.log(error))
+        if (this.state.products.length === 0) {
+            this.loadProduct(this.state.page);
+        }
+    }
+
+    onNext = () => {
+        const { page, shouldNavigatePage } = this.state;
+        if (shouldNavigatePage) {
+            this.loadProduct(page + 1);
+            this.setState({ page: page + 1 });
+        }
+    }
+
+    onPrevious = () => {
+        const { page, shouldNavigatePage } = this.state;
+        if (shouldNavigatePage && page > 0) {
+            this.loadProduct(page);
+            this.setState({ page: page - 1 })
+        }
+
     }
 
     render() {
-        console.log("List products:",this.state.products)
+        const { page } = this.state;
         return (
             <div>
                 <Navbar />
@@ -33,10 +61,17 @@ class ManageProduct extends Component {
                     <div>
                         <Link to="/products/new" className="btn btn-default" id="btnCreateUser">Create Product</Link>
                     </div>
-                    <ProductTable listProducts={this.state.products}/>
+                    <div>
+                        <span className="btn btn-info">Page: {page > 0 ? page : (page + 1)}</span>
+                    </div>
+                    <ProductTable listProducts={this.state.products} />
+                    <div>
+                        <span className="btn btn-info">Page: {page > 0 ? page : (page + 1)}</span>
+                    </div>
                     <nav aria-label="...">
                         <ul className="pagination pagination-lg">
-                            <li className="page-item"><a className="page-link" href="#none" >1</a></li>
+                            <li className="page-item"><button onClick={this.onPrevious} className="page-link" href="#none" >Previous</button></li>
+                            <li className="page-item"><button onClick={this.onNext} className="page-link" href="#none" >Next</button></li>
                         </ul>
                     </nav>
                 </div>
