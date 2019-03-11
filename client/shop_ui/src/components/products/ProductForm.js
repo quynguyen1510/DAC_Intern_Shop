ab312d40b7e3eb6b3b7a79dfc4ff655b46c5afdc
lap14tclc2 <honaz140@gmail.com>
2019-03-11 12:02:19 +0700

    combine api in ProductForm

--- client/shop_ui/src/components/products/ProductForm.js
+++ client/shop_ui/src/components/products/ProductForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { addNewProduct } from '../../api/product_api';
-
+import { uploadImage } from '../../api/imgur_api';
+import {withRouter} from 'react-router';
+var jwt_decode = require('jwt-decode');
 class FormCreateProduct extends Component {
     constructor(props) {
         super(props);
@@ -11,9 +13,27 @@ class FormCreateProduct extends Component {
             price: '',
             category_id: '',
             product_img: '',
-            user_id: 1
+
+            // error
+            nameError: '',
+            descError: '',
+            priceError: ''
         }
     }
+
+    validateNameProduct = () =>{
+        this.setState({nameError: (this.state.product_name.length === 0 ? "You should input something cool" :"") });
+    }
+
+    validateDescProduct = () =>{
+        this.setState({descError: (this.state.product_desc.length === 0 ? "You should input something cool" :"") })
+    }
+
+    validatePrice = () =>{
+        this.setState({priceError: (Number(this.state.price) === 0 ? "Product description must be greater than 0" :"") })
+    }
+
+
     handleProductNameChange = event => {
         this.setState({ product_name: event.target.value });
     }
@@ -29,20 +49,43 @@ class FormCreateProduct extends Component {
     handleCategoryChange = event => {
         this.setState({ category_id: event.target.value })
     }
+
+    uploadImage =(event) =>{
+        const imgFile = event.target.files[0];
+        uploadImage(imgFile).then(response => {
+            const imageUrl = `https://i.imgur.com/${response.data.data.id}.png`;
+            this.setState({product_img: imageUrl})
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     handleSubmit = () => {
-        const { product_name, product_desc,price , category_id, user_id, product_img } = this.state;
+        const { product_name, product_desc,price , category_id, product_img } = this.state;
+        const shouldSendRequest = this.state.product_name.length > 0 || this.state.product_desc > 0 || this.state.price.length > 0;
+        if(!shouldSendRequest){
+            alert("You must change something");
+            return;
+        }
         const token = localStorage.getItem("token");
+        const payload = jwt_decode(token);
         const product = {
             "product_name" : `${product_name}`,
             "product_desc" : `${product_desc}`,
             "price" : `${price}`,
             "product_img" : `${product_img}`,
-            "category_id": `${category_id}`,
-            "user_id": `${user_id}`
+            "category_id": `${category_id ? category_id : 1}`,
+            "user_id": `${payload.user_id}`
         }
-        addNewProduct(token,product)
+        if(this.props.currentProduct === undefined){
+            addNewProduct(token,product);
+            this.props.history.push('/manage/products')
+        }
     }
+
     render() {
+        const {currentProduct, categories} = this.props;
+    
         return (
             <div>
                 <form encType="multipart/form-data">
@@ -51,35 +94,53 @@ class FormCreateProduct extends Component {
                             <label className="col-form-label">Product Name</label>
                             <input type="text"
                                 name="product_name"
+                                defaultValue={currentProduct ? currentProduct.product_name: ""}
                                 className="form-control"
                                 onChange={this.handleProductNameChange}
+                                onBlur={this.validateNameProduct}
                             />
                         </div>
-                        <div className="invalid-feedback"></div>
+                        <div className="invalid-feedback">{this.state.nameError}</div>
                     </div>
 
                     <div className="form-group row justify-content-center">
                         <div className="col-sm-6">
                             <label className="col-form-label">Product Description</label>
-                            <input onChange={this.handleProductDescChange} type="text" name="product_desc" className="form-control" />
-                            <div className="invalid-feedback"></div>
+                            <input onChange={this.handleProductDescChange}
+                                    type="text"
+                                    name="product_desc" 
+                                    defaultValue={currentProduct ? currentProduct.product_desc: ""}
+                                    className="form-control"
+                                    onBlur={this.validateDescProduct} />
+                            <div className="invalid-feedback">{this.state.descError}</div>
                         </div>
                     </div>
                     <div className="form-group row justify-content-center">
                         <div className="col-sm-6">
                             <label className="col-form-label">Product Price</label>
-                            <input onChange={this.handlePriceChange} type="number" className="form-control" name="price" />
+                            <input onChange={this.handlePriceChange} 
+                                    type="number" 
+                                    className="form-control" 
+                                    name="price"
+                                    onBlur={this.validatePrice}
+                                    defaultValue={currentProduct ? currentProduct.price: ""} />
                         </div>
-                        <div className="invalid-feedback"></div>
+                        <div className="invalid-feedback">{this.state.priceError}</div>
                     </div>
 
                     <div className="form-group row justify-content-center">
                         <div className="col-sm-6">
                             <label className="col-form-label">Category Name</label>
-                            <select className="form-control" onChange={this.handleCategoryChange}>
-                                <option value="1">Điện tử</option>
-                                <option value="2">Đồ mẹ & bé</option>
-                                <option value="3">Du lịch</option>
+                            <select className="form-control" 
+                                    onChange={this.handleCategoryChange}
+                                    defaultValue={currentProduct ? currentProduct.category_name: ""}>
+                                {
+                                    categories.length > 0 && (
+                                        categories.map((cate, index) => {
+                                            return (<option key={index} value={cate.id}>{cate.catname}</option>)
+                                        })
+                                    )
+                                }
                             </select>
                         </div>
                         <div className="invalid-feedback"></div>
@@ -89,19 +150,28 @@ class FormCreateProduct extends Component {
                         <div className="col-sm-6">
                             <label className="col-form-label">Product Image</label>
                             <div className="col-sm-12">
-                                <input type="file" className="custom-file-input" id="uploadImage" />
-                                <label htmlFor="uploadImage" className="custom-file-label">Choose File</label>
+                                <input type="file" 
+                                        onChange={this.uploadImage}
+                                        className="custom-file-input" 
+                                        id="uploadImage" />
+                                {
+                                    this.state.product_img ? <label htmlFor="uploadImage" className="custom-file-label">{this.state.product_img}</label> :
+                                    <label htmlFor="uploadImage" className="custom-file-label">Choose File</label>
+                                }
                             </div>
                         </div>
                     </div>
 
                 </form>
                 <div className="submit-profile justify-content-center">
-                    <button onClick={this.handleSubmit} className="btn btn-primary update-profile-button">Create</button>
+                    <button onClick=
+                        {this.handleSubmit} 
+                        className="btn btn-primary update-profile-button">
+                        {currentProduct ? "Update" : "Create"}</button>
                 </div>
             </div>
         );
     }
 }
 
-export default FormCreateProduct;
+export default withRouter(FormCreateProduct);
