41ed89d87f08626a5864f57b6f05f8eabd9adde9
lap14tclc2 <honaz140@gmail.com>
2019-03-11 12:00:06 +0700

    get Categories to render select Categories element

--- client/shop_ui/src/components/products/CreateProduct.js
+++ client/shop_ui/src/components/products/CreateProduct.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import ProductForm from './ProductForm';
 import Navbar from '../commons/header/Navbar';
+import { getCategories } from '../../api/product_api';
 
 class FormCreateProduct extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            categories: []
+        }
+    }
+    componentDidMount(){
+        getCategories().then(res => {
+            this.setState({categories: [...res.data]});
+        }).catch(err =>{
+            console.log(err);
+        })
+    }
+
     render() {
         return (
             <div className="detail">
               <Navbar /> 
             <h2 className="text-center"> Create New Product</h2>
             <div className="inputForm">
-                <ProductForm />
+                <ProductForm categories={this.state.categories}/>
             </div>
         </div>
         );
--- client/shop_ui/src/components/products/ProductInfor.js
+++ client/shop_ui/src/components/products/ProductInfor.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import ProductForm from './ProductForm';
 import Navbar from '../commons/header/Navbar';
+import { getProductById, getCategories } from '../../api/product_api';
 
 class ProductInfor extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentProduct: null,
+            categories: []
+        }
+    }
+    componentDidMount(){
+        const productId = this.props.match.params.product_id;
+        getProductById(productId).then(response => {
+            this.setState({currentProduct: response.data});
+        }).catch(err => {
+            console.log(err);
+        });
+
+        getCategories().then(res => {
+            this.setState({categories: [...res.data]});
+        }).catch(err =>{
+            console.log(err);
+        })
+    }
     render() {
+        const { currentProduct, categories} = this.state;
         return (
             <div>
                 <Navbar />
                 <div className="profile">
-                    <h2>Samsung Galaxy S10+</h2>
+                    <h2>{currentProduct ? currentProduct.product_name: ""}</h2>
                     <div className="avatar">
-                        <img className="avatar-default" src="https://picsum.photos/100/100/?random" alt="" />
+                        <img className="avatar-default" src={currentProduct ? currentProduct.product_img : ""} alt="" />
                     </div>
                     <div className="inputForm">
-                        <ProductForm />
+                        <ProductForm currentProduct = {this.state.currentProduct} categories={categories}/>
                     </div>
                 </div>
             </div>
