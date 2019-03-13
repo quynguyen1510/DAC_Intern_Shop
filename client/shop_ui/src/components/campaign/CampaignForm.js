a7c5e64a6502142e6ad02f1eef0099e65b601990
lap14tclc2 <honaz140@gmail.com>
2019-03-13 16:44:32 +0700

    validate field and handle event in CampaignForm

--- client/shop_ui/src/components/campaign/CampaignForm.js
+++ client/shop_ui/src/components/campaign/CampaignForm.js
@@ -1,7 +1,80 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-
+import {BID_AMOUNT, CAMPAIGN_ACTIVE, CAMPAIGN_UN_ACTIVED} from '../../util/constant';
+import { uploadImage } from '../../api/imgur_api';
 class FormCreateCampaign extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            status: CAMPAIGN_ACTIVE,
+            startdate: '',
+            enddate: '',
+            budget: '',
+            title: '',
+            description: '',
+            campaignimg : '',
+            final_url: ''
+        }
+    }
+
+    handleChangeName = event => {
+        this.setState({ name: event.target.value});
+    }
+
+    handleChangeStatus = event => {
+        this.setState({ status: event.target.value});
+    }
+
+    handleChangeStartDay = event => {
+        this.setState({ startdate: event.target.value});
+    }
+
+    handleChangeEndDay = event => {
+        this.setState({ enddate: event.target.value});
+    }
+
+    handleChangeBudget = event => {
+        this.setState({ budget: event.target.value});
+    }
+
+    handleChangeTitle = event => {
+        this.setState({ title: event.target.value});
+    }
+
+    handleChangeDesc = event => {
+        this.setState({ description: event.target.value});
+    }
+
+    handleChangeUrlFor = event => {
+        this.setState({ final_url: event.target.value});
+    }
+    
+    uploadImage = event  =>{
+        const imgFile = event.target.files[0];
+        uploadImage(imgFile).then(response => {
+            const imageUrl = `https://i.imgur.com/${response.data.data.id}.png`;
+            this.setState({campaignimg: imageUrl})
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const newCampaign = {};
+        const attributes = Object.keys(this.state);
+        for(let index in attributes){
+            const key = attributes[index];
+            const value = this.state[`${attributes[index]}`];
+            console.log(value)
+           if(value){
+               newCampaign[key] = value;
+           }
+        }
+        newCampaign.bid = BID_AMOUNT
+        console.log(newCampaign)
+    }
 
     render() {
         return (
@@ -12,7 +85,7 @@ class FormCreateCampaign extends Component {
                             <h4>Create Campaign</h4>
                             <hr />
                             <div className="campaign-content">
-                                <form>
+                                <form onSubmit={this.handleSubmit} >
                                     {/* ============================================Detail======================================= */}
                                     <div className="card">
                                         <div className="card-header custom-card-header" data-toggle="collapse" data-target="#collapseDetail" aria-expanded="false" aria-controls="collapseDetail">
@@ -28,15 +101,23 @@ class FormCreateCampaign extends Component {
                                                 <div className="form-group row custom-form-row">
                                                     <label className="col-4 col-form-label">Name :</label>
                                                     <div className="col-8 custom-style">
-                                                        <input type="text" name="name" placeholder="Name" className="form-control" required />
+                                                        <input type="text" 
+                                                                name="name" placeholder="Name" 
+                                                                className="form-control" 
+                                                                defaultValue=""
+                                                                required
+                                                                onChange={this.handleChangeName}/>
                                                     </div>
                                                 </div>
                                                 <div className="form-group row custom-form-row">
-                                                    <label className="col-4 col-form-label">User Status :</label>
+                                                    <label className="col-4 col-form-label">Campain Status: </label>
                                                     <div className="col-8 custom-style">
-                                                        <select name="status" className="form-control">
-                                                            <option value="ACTIVE">ACTIVE</option>
-                                                            <option value="PAUSE">PAUSE</option>
+                                                        <select name="status" 
+                                                                className="form-control"
+                                                                onChange={this.handleChangeStatus}
+                                                                value={this.state.status}>
+                                                            <option value={CAMPAIGN_ACTIVE}>ACTIVE</option>
+                                                            <option value={CAMPAIGN_UN_ACTIVED}>UNACTIVED</option>
                                                         </select>
                                                     </div>
                                                 </div>
@@ -56,18 +137,26 @@ class FormCreateCampaign extends Component {
                                         <div className="collapse" id="collapseSchedule">
                                             <div className="card-body">
                                                 <div className="form-group row custom-form-row">
-                                                    <label className="col-4 col-form-label">Start date :</label>
+                                                    <label className="col-4 col-form-label">Start date: </label>
                                                     <div className="col-8 custom-style">
                                                         <div className="input-left">
-                                                            <input type="date" name="name" className="form-control" required />
+                                                            <input type="date" 
+                                                            name="startDay" 
+                                                            className="form-control"  
+                                                            onChange={this.handleChangeStartDay}
+                                                            required />
                                                         </div>                                                   
                                                     </div>
                                                 </div>
                                                 <div className="form-group row custom-form-row">
-                                                    <label className="col-4 col-form-label">End date :</label>
+                                                    <label className="col-4 col-form-label">End date: </label>
                                                     <div className="col-8 custom-style">
                                                         <div className="input-left">
-                                                            <input type="date" name="name" className="form-control" required />
+                                                            <input type="date" 
+                                                            name="endDay" 
+                                                            className="form-control" 
+                                                            onChange={this.handleChangeEndDay}
+                                                            required />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -95,7 +184,13 @@ class FormCreateCampaign extends Component {
                                                                     <i className="fa fa-dollar-sign" />
                                                                 </span>
                                                             </div>
-                                                            <input type="number" name="budget" placeholder="Budget" min={1} className="form-control" required />
+                                                            <input type="number" 
+                                                            name="budget" 
+                                                            placeholder="Budget" 
+                                                            min={1} 
+                                                            className="form-control" 
+                                                            onChange={this.handleChangeBudget}
+                                                            required />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -123,7 +218,13 @@ class FormCreateCampaign extends Component {
                                                                     <i className="fa fa-dollar-sign" />
                                                                 </span>
                                                             </div>
-                                                            <input type="number" name="bid" placeholder="Bidding Amount" min={1} className="form-control" required />
+                                                            <input type="number" 
+                                                            name="bid" 
+                                                            placeholder="Bidding Amount" 
+                                                            min={1} 
+                                                            value={10}
+                                                            readOnly
+                                                            className="form-control"/>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -145,28 +246,47 @@ class FormCreateCampaign extends Component {
                                                 <div className="form-group row custom-form-row">
                                                     <label className="col-4 col-form-label">Title :</label>
                                                     <div className="col-8">
-                                                        <input type="text" name="title" placeholder="Title" className="form-control" required />
+                                                        <input type="text" 
+                                                        name="title" 
+                                                        placeholder="Title" 
+                                                        className="form-control" 
+                                                        onChange={this.handleChangeTitle}
+                                                        required />
                                                     </div>
                                                 </div>
                                                 <div className="form-group row custom-form-row">
                                                     <label className="col-4 col-form-label">Description :</label>
                                                     <div className="col-8">
-                                                        <textarea name="description" placeholder="Description" className="form-control" rows="4" defaultValue={""} />
+                                                        <textarea name="description" 
+                                                        placeholder="description" 
+                                                        className="form-control" 
+                                                        rows="4" 
+                                                        defaultValue={""}
+                                                        onChange={this.handleChangeDesc} />
                                                     </div>
                                                 </div>
                                                 <div className="form-group row custom-form-row">
                                                     <label className="col-4 col-form-label">Create preview :</label>
                                                     <div className="col-8 custom-style">
-                                                        <input type="file" accept="images/*" />
+                                                        <input type="file" accept="images/*"  onChange={this.uploadImage} required/>
                                                     </div>
-                                                    <div className="offset-4 col-8 img-preview">
-                                                        <img src="https://cdn.wallpapersafari.com/53/47/4YNVas.jpg" alt="none" />
-                                                    </div>
+                                                    {
+                                                        this.state.campaignImageUrl && (
+                                                            <div className="offset-4 col-8 img-preview">
+                                                                <img src={this.state.campaignImageUrl} alt="none" />
+                                                            </div>
+                                                        )
+                                                    }
                                                 </div>
                                                 <div className="form-group row custom-form-row">
                                                     <label className="col-4 col-form-label">Final URL :</label>
                                                     <div className="col-8">
-                                                        <input type="text" name="finalURL" placeholder="Final URL" className="form-control" required />
+                                                        <input type="text" 
+                                                        name="finalURL" 
+                                                        placeholder="Final URL" 
+                                                        className="form-control" 
+                                                        onChange={this.handleChangeUrlFor}
+                                                        required />
                                                     </div>
                                                 </div>
                                             </div>
