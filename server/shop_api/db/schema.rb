8a3adddd392d750aa994b913241aa669ea02f500
lap14tclc2 <honaz140@gmail.com>
2019-03-13 16:45:12 +0700

    campaign new schema

--- server/shop_api/db/schema.rb
+++ server/shop_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_043849) do
+ActiveRecord::Schema.define(version: 2019_03_13_093129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2019_03_13_043849) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "title"
+    t.string "description"
+    t.string "final_url"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
