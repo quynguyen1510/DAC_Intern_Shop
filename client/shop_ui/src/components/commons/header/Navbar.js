1531e6bb31818043ec0b9266ab5a9ad15fee04ce
quynguyen1510 <quycao1510@gmail.com>
2019-03-18 10:07:15 +0700

    Add menu for navbar

--- client/shop_ui/src/components/commons/header/Navbar.js
+++ client/shop_ui/src/components/commons/header/Navbar.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ADMIN_ROLE, SHOPPER_ROLE } from '../../../util/constant';
+var jwtDecode = require('jwt-decode');
 
 class Navbar extends Component {
     render() {
+        const token = localStorage.getItem('token');
+        const currentUser  = jwtDecode(token);
+      
         return (
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link to={'/'} className="nav-link" id="nav-cus" href="#none">Home</Link>
-                    </li>
-                </ul>
+                <div className="btn-group dropright">
+                    <button type="button" className="btn btn-secondary">
+                        <Link to={'/'} className="nav-link" id="nav-cus">Home</Link>
+                    </button>
+                    <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span className="sr-only">Toggle Dropright</span>
+                    </button>
+                    <div className="dropdown">
+                        <ul className="dropdown-menu nav-cus-menu">
+                            <li className="dropdown-item">
+                                {
+                                    currentUser ? <Link to={`/profile/users/${currentUser.user_id}`} >Thông tin cá nhân</Link> : null
+                                }
+                            </li>
+                            <li className="dropdown-item ">
+                                {
+                                    currentUser ? (currentUser.role_id === ADMIN_ROLE ? <Link to="/manage/users/1" >Quản lý tài khoản</Link> : null) : null
+                                }
+                            </li>
+                            <li className="dropdown-item ">
+                                {
+                                    currentUser &&
+                                    ((currentUser.role_id === ADMIN_ROLE || currentUser.role_id === SHOPPER_ROLE) ?
+                                        <Link to="/manage/products" >Quản lý sản phẩm</Link> : null)
+                                }
+                            </li>
+                            <li className="dropdown-item">
+                                {
+                                    currentUser &&
+                                    ((currentUser.role_id === ADMIN_ROLE || currentUser.role_id === SHOPPER_ROLE) ?
+                                        <Link to="/manage/campaign" >Quản lý chiến dịch</Link> : null)
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         );
     }
