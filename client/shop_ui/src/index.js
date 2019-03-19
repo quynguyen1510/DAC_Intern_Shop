a8648a0103e8759488e0ac87e95480e3dd034770
lap14tclc2 <honaz140@gmail.com>
2019-03-19 15:23:59 +0700

    remove redux code in index

--- client/shop_ui/src/index.js
+++ client/shop_ui/src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {store, persistor} from './store/ConfigStore';
-import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import {PersistGate} from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import ProtectedRoutesContainer from './routes/ProtectedRoute';
 
 
-ReactDOM.render(<Provider store={store}>
-                    <PersistGate loading={null} persistor={persistor}>
-                        <BrowserRouter >
-                            <ProtectedRoutesContainer/>
-                        </BrowserRouter>
-                    </PersistGate>
-                </Provider>, 
-document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter >
+        <ProtectedRoutesContainer />
+    </BrowserRouter>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
