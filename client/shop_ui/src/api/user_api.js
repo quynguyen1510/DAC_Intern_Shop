1b8aeeebf8bbb74c1bbf5fcb260993c94971099a
lap14tclc2 <honaz140@gmail.com>
2019-03-19 11:14:38 +0700

    add user_api and change name of campaign_api

---
+++ client/shop_ui/src/api/user_api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {HEROKU_API_URL} from '../util/constant';
+
+
+export async function create(user){
+    const url = `${HEROKU_API_URL}/users`;
+    return await axios.post(url, user, {headers: configHeader()})
+}
+
+export async function update(user, id){
+    const url = `${HEROKU_API_URL}/users/${id}`;
+    return await axios.put(url, user, {headers: configHeader()});
+}
+export async function getUsers(page){
+    const url = `${HEROKU_API_URL}/users?page=${page}`;
+    return await axios.get(url, {headers: configHeader()})
+}
+
+export async function deleteUser(id){
+    const url = `${HEROKU_API_URL}/users/${id}`;
+    return await axios.delete(url, {headers: configHeader()})
+}
+
+
+function configHeader(){
+    const token = localStorage.getItem("token");
+    return {
+        "Authorization": token,
+        'content-type': 'application/json'
+    }
+}
