121b39c6cb0f312a2d7d64a17bb33447a3880a8f
lap14tclc2 <honaz140@gmail.com>
2019-03-25 13:43:59 +0700

    display number items in cart

--- client/shop_ui/src/components/commons/header/Cart.js
+++ client/shop_ui/src/components/commons/header/Cart.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-
 class Cart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            numberItems: 0,
+        }
+    }
+
+    componentDidMount(){
+        // declare cart 
+        let cart = localStorage.getItem("cart");
+        if(cart == null || cart.length === 0){
+            cart = []
+            localStorage.setItem("cart", JSON.stringify([]));
+        }
+        else{
+            this.setState({numberItems: JSON.parse(localStorage.getItem("cart")).length})
+        }
+    }
+
     render() {
         return (
             <div className="col-md-1 nav-cart">
-                <a href="#top">
+                <a href="#">
                     <img src="https://img.icons8.com/ios/50/000000/add-shopping-cart.png" alt="Giỏ hàng" />
                 </a>
+                <p className="cart-number">                     
+                    {
+                        `(${this.state.numberItems})`
+                    }
+                </p>
             </div>
         );
     }
